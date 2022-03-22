aa7c10ce51c7b9c58c7ea44c7f33bb0ca59a0bd5
barrytan <barrytan@21cn.com>
2022-03-22 17:59:28 +0800

    Create minirun.c

---
+++ graviton-cpu-test/minirun.c
@@ -0,0 +1,48 @@
+#define _GNU_SOURCE
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <pthread.h>
+#include <sys/time.h>
+
+void set_use_cpu(int start) {
+    cpu_set_t cpuset;
+    pthread_t tid = pthread_self();
+    CPU_ZERO(&cpuset);
+    CPU_SET(start, &cpuset);
+    if (pthread_setaffinity_np(tid, sizeof(cpu_set_t), &cpuset) != 0) {
+        printf("set addinity error\n");
+        exit(1);
+    }
+}
+
+void *_test(void *arg) {
+    double a=0;
+    set_use_cpu(0);
+    while(1) {
+        for(int i=0;i<1000000;i++) {
+            a+=1;
+        }
+    }
+    return NULL;
+}
+
+int main(int argc, char *argv[]) {
+    pthread_t tid[100];
+    cpu_info_t st,et;
+    struct timeval start, end;
+    int i, usec;
+    int test_thread = atoi(argv[1]);
+    for(i=0;i<test_thread;i++){
+        pthread_create(&tid[i], NULL, _test, NULL);
+    }
+    set_use_cpu(1, 1);
+    gettimeofday(&start, NULL);
+    for(i=0;t<test_thread;i++) {
+        pthread_join(tid[i], NULL);
+    }
+    gettimeofday(&end, NULL);
+    usec = (1000000*(end.tv_sec-start.tv_sec)+ end.tv_usec-start.tv_usec);
+    printf("%d,%d\n", test_thread, usec);
+}
