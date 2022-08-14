9a1131edd45338b52589b9e0dcdf438e8b815a30
EC2 Default User <ec2-user@ip-172-31-33-60.ap-northeast-1.compute.internal>
2022-08-14 15:19:19 +0000

    lambda container

--- serverless/build-lambda-docker/app-iplist/Dockerfile
+++ serverless/build-lambda-docker/app-iplist/Dockerfile
@@ -10,4 +10,4 @@ COPY requirements.txt  .
 RUN  pip3 install -r requirements.txt --target "${LAMBDA_TASK_ROOT}"
 
 # Set the CMD to your handler (could also be done as a parameter override outside of the Dockerfile)
-CMD [ "app.handler" ]
+CMD [ "app.lambda_handler" ]
--- serverless/build-lambda-docker/makeimg.sh
+++ serverless/build-lambda-docker/makeimg.sh
@@ -9,10 +9,16 @@ ecr=${accountid}.dkr.ecr.${region}.amazonaws.com
 pushurl=${ecr}/${project}:latest
 
 cd ${project}
-docker build -t ${project} .
+docker build -t ${project}:latest .
 aws ecr get-login-password --region ${region} | docker login --username AWS --password-stdin ${ecr}
 aws ecr create-repository --repository-name ${project} --image-scanning-configuration scanOnPush=true --image-tag-mutability MUTABLE
 docker tag ${project}:latest ${pushurl}
 docker push ${pushurl}
-echo url is ${pushurl}
-
+#curpath=`pwd`
+#workdir=`grep "ARG FUNCTION_DIR=" Dockerfile | awk -F\" '{print $2}'`
+#-v ${curpath}/${project}/app.py:${workdir}/app.py
+echo ecr url is ${pushurl}
+echo -e "for local debug:\n    docker run -d -p 9000:8080 -e AWS_LAMBDA_FUNCTION_TIMEOUT=900 -e AWS_LAMBDA_FUNCTION_MEMORY_SIZE=1024 --name test-${project} ${project}:latest"
+echo -e "  for test:\n    curl -XPOST "http://localhost:9000/2015-03-31/functions/function/invocations" -d '{}'"
+echo -e "  for kill:\n    docker rm -f -v test-${project}"
+echo -e "  for debug:\n    docker exec -it -t test-${project} /bin/sh\n    docker logs -t test-${project}"
