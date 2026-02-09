f2f52d1950623ded1f06d5eb9a9f163bcdd23ac0
barrytan <20962493@qq.com>
2026-02-09 16:51:11 +0000

    stress logic

---
+++ dynamodb-stress-test/sample-stress-test-row.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+region=us-east-1
+
+go build -o stress-test stress-test.go
+
+ctable=stress-test-multirow
+
+# 测试数据量1TB，100K 预置 WCU/RCU 的性能
+aws dynamodb create-table \
+  --table-name ${ctable} \
+  --attribute-definitions AttributeName=id,AttributeType=S \
+      AttributeName=sk,AttributeType=S \
+  --key-schema AttributeName=id,KeyType=HASH \
+      AttributeName=sk,KeyType=RANGE \
+  --tags Key=Cost-Center,Value=stress-test \
+  --billing-mode PROVISIONED \
+  --provisioned-throughput ReadCapacityUnits=100000,WriteCapacityUnits=100000 \
+  --region ${region}
+
+aws dynamodb create-table \
+  --table-name ${ctable}10 \
+  --attribute-definitions AttributeName=id,AttributeType=S \
+      AttributeName=sk,AttributeType=S \
+  --key-schema AttributeName=id,KeyType=HASH \
+      AttributeName=sk,KeyType=RANGE \
+  --tags Key=Cost-Center,Value=stress-test \
+  --billing-mode PROVISIONED \
+  --provisioned-throughput ReadCapacityUnits=100000,WriteCapacityUnits=100000 \
+  --region ${region}
+
+aws dynamodb wait table-exists --table-name ${ctable} --region ${region}
+aws dynamodb wait table-exists --table-name ${ctable}10 --region ${region}
+
+# 确保表预热完成
+# sleep 600
+
+# 插入1TB数据
+# times = 1000000000 KB / 5KB item-size / 1 batch / 20 threads / 1 ec2
+./start-stress.sh 1 -table ${ctable} -t 86400 -batchWriteItem 20 -region ${region} -config package-size.yaml -times 10000000
+# times = 10000000000 KB / 5KB item-size / 1 batch / 20 threads / 1 ec2
+./start-stress.sh 1 -table ${ctable} -t 86400 -batchWriteItem 20 -region ${region} -config package-size.yaml -times 100000000
+
